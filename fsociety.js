aaa9c29c9d0bd4458f834cdadeeb3db35f4b2d3d
Alex Kaufman <alexkaufman06@gmail.com>
2016-09-24 11:40:53 -0700

    Add initial file

---
+++ fsociety.js
@@ -0,0 +1,4 @@
+var path = require("path");
+var name = "Alex"
+console.log(`<mr. robot>I see you are interested in joining the revolution. What's your name? ~${path.basename(__filename)}`);
+console.log(`<mr. robot>Hello there ${name}, are you are you seeing this too?`);
